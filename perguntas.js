979e84cd5fa844a493a60031140aed62e75bbcfa
Wagnerdettona <160977524+Wagnerdettona@users.noreply.github.com>
2024-10-02 19:47:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Mecanica',
+    'Quantos parafusos tem na bomba d´agua do moza 93?',
+    'Somente 3 parafusos'
+)
+
+criaCartao(
+    'Mecanica 2',
+    'Quantos CV tem um moza E.F.I?',
+    'Carro vem originalmente com 110cv'
+)
+
+criaCartao(
+    'Mecanica',
+    'Quantos litros de oléo vai no motor do monza?',
+    'Motor ultiliza 4,5 Litros'
+)
+
+criaCartao(
+    'Mecanica',
+    'Quantos litros tem o porta malas do monza?',
+    'Porta malas tem 565 Litros'
+)
+
+criaCartao(
+    'Mecanica',
+    'Qual numero da furação da roda do Monza?',
+    'Furação é 4x100'
+)
+
+criaCartao(
+    'Mecanica',
+    'Qual oléo ultilizado no motor GM do monza?',
+    'Motor usa o oléo 20w50'
+)
+
+criaCartao(
+    'Mecanica',
+    'Qual peso do monza 1993?',
+    'Modelo 1993 pesa apenas 1.062 kilos'
+)
+
+criaCartao(
+    'Mecanica',
+    'Quantos litros vai no tanque do monza? ',
+    'Contem reservatorio para 57 litros'
+)
+
+criaCartao(
+    'Mecanica',
+    'Qual comprimento do monza?',
+    'Carro mede de ponta a ponta 4,49m'
+)
+
+criaCartao(
+    'Mecanica',
+    'Qual largura entre eixos do monza?',
+    'Largura é de 2,57m'
+)
